84e480f6c17f05a4ae7e3c7936314d7f0622452b
CZNOR <hugo@agencemade.com>
2026-03-23 03:41:39 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ import AgendaPage from "@/pages/AgendaPage";
 import TeamPage from "@/pages/TeamPage";
 import GoalsPage from "@/pages/GoalsPage";
 import GamificationPage from "@/pages/GamificationPage";
+import VentesPage from "@/pages/VentesPage";
+import FinancesPage from "@/pages/FinancesPage";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -34,6 +36,8 @@ const App = () => (
             <Route path="/team" element={<TeamPage />} />
             <Route path="/goals" element={<GoalsPage />} />
             <Route path="/gamification" element={<GamificationPage />} />
+            <Route path="/ventes" element={<VentesPage />} />
+            <Route path="/finances" element={<FinancesPage />} />
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
